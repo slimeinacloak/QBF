0441e8469a2898c4af8718d1da623335ae0f1d1b
sweetbaboo-1 <72481120+sweetbaboo-1@users.noreply.github.com>
2025-06-19 21:58:44 -0700

    Fix XaeroMapWaypointCreator

--- src/main/java/org/peartcraft/XaeroMapWaypointCreator.java
+++ src/main/java/org/peartcraft/XaeroMapWaypointCreator.java
@@ -8,11 +8,16 @@ import java.util.List;
 public class XaeroMapWaypointCreator implements WayPointCreator {
     @Override
     public List<String> createWaypoints(List<SchematicBlockPos> positions, ConfigData data) {
+        int x, y, z;
+        x = Math.min(data.x1(), data.x2());
+        y = Math.min(data.y1(), data.y2());
+        z = Math.min(data.z1(), data.z2());
+
         List<String> waypoints = new ArrayList<>();
 
         int i = 0;
         for (SchematicBlockPos pos : positions) {
-            waypoints.add(String.format("waypoint:%d:UB:%d:%d:%d:0:false:0:gui.xaero_default:false:0:0", i, pos.x, pos.y, pos.z));
+            waypoints.add(String.format("waypoint:%d:UB:%d:%d:%d:0:false:0:gui.xaero_default:false:0:0:true", i, pos.x + x, pos.y + y, pos.z + z));
             i++;
         }
 
